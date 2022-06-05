355fddfd505d880932b529bfcd49b194e050a008
sapui5karthik <76442240+sapui5karthik@users.noreply.github.com>
2022-06-05 13:11:33 +0530

    Update App.controller.js

--- project1/webapp/controller/App.controller.js
+++ project1/webapp/controller/App.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
     return BaseController.extend("com.wip2.project1.controller.App", {
 
         onInit : function () {
-            // am-1
+            // am-2
             // apply content density mode to root view
             // Adding comments
             // suman
